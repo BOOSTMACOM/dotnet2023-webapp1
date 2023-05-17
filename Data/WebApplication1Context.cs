3bc67537a28437ea19074e7facb4993935f4f7d8
Théo <boostmacom@gmail.com>
2023-05-17 11:27:43 +0200

    Ajout du système d'authentification via Identity

--- Data/WebApplication1Context.cs
+++ Data/WebApplication1Context.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Models;
 
 namespace WebApplication1.Data
 {
-    public class WebApplication1Context : DbContext
+    public class WebApplication1Context : IdentityDbContext<IdentityUser>
     {
         public WebApplication1Context (DbContextOptions<WebApplication1Context> options)
             : base(options)
--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using WebApplication1.Data;
 using WebApplication1.Domain.Services;
 using WebApplication1.Repositories;
 using WebApplication1.Repositories.Interface;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns;
 
 namespace WebApplication1
 {
@@ -14,6 +16,9 @@ namespace WebApplication1
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddDbContext<WebApplication1Context>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("WebApplication1Context") ?? throw new InvalidOperationException("Connection string 'WebApplication1Context' not found.")));
+
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<WebApplication1Context>();
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
@@ -34,12 +39,27 @@ namespace WebApplication1
             app.UseStaticFiles();
 
             app.UseRouting();
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+
+                endpoints.MapRazorPages();
+            });
+
+            //app.MapControllerRoute(
+            //    name: "default",
+            //    pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
